e35bdea13363b6c5935df82af658aa003d88918e
Josette Garcia <josettetgarcia@gmail.com>
2022-02-16 23:05:05 -0600

    commit tree 5559af45ff1d2bc74433df47d660b967e9d662a5

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+
+const theURL = "http://localhost:3000/users"
+
+const configurationObject = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify({
+    name : "Steve",
+    email : "steve@steve.com",
+  }),
+};
+
+function submitData(userName, userEmail){
+  return fetch(theURL, configurationObject)
+    .then(response => response.json())
+    .then(getID)
+    .catch(errorFunc)
+}
+
+function getID(object){
+  document.body.innerHTML = object.id
+}
+
+function errorFunc(error){
+  document.body.innerHTML = error.message
+}
+
+
